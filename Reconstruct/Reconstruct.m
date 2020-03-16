1eb95ccb417651cc8d6fdd6fe6ef0b576a849aca
Salman Masoumi <salman412412@gmail.com>
2020-03-16 20:45:25 +0330

    Update Reconstruct.m

--- Reconstruct/Reconstruct.m
+++ Reconstruct/Reconstruct.m
@@ -14,6 +14,7 @@ phi0 = H(:,3)*pi/180;
 for i = 1:size(t,2)
     Theta = omega*t(1,i) + phi0;
     Y_re = A.*cos(Theta);
-    Y(i) = sum(Y_re);
+    Y_im = (1i*A).*sin(Theta);
+    Y(i) = sum(Y_re) + sum(Y_im);
 end
-plot(t,Y)
+plot(t,real(Y))
