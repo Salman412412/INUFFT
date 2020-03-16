6c4ba57d23ae12562ef3a46a40236d74f7c7b904
Salman Masoumi <salman412412@gmail.com>
2020-03-16 20:02:35 +0330

    Create Reconstruct.m

---
+++ Reconstruct/Reconstruct.m
@@ -0,0 +1,19 @@
+clear, clc
+load healthy.mat
+H = table2array(healthy);
+omega = 2*pi*H(:,1);
+fmin = min(H(:,1));
+minDAQ = 10;
+tmax = minDAQ/fmin;
+fmax = max(H(:,1));
+dt = tmax/(minDAQ * fmax);
+t = 0:dt:tmax;
+Y = zeros(size(t));
+A = abs(H(:,2));
+phi0 = H(:,3)*pi/180;
+for i = 1:size(t,2)
+    Theta = omega*t(1,i) + phi0;
+    Y_re = A.*cos(Theta);
+    Y(i) = sum(Y_re);
+end
+plot(t,Y)
